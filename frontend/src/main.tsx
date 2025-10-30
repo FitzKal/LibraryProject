ea5d32603d1c74dee90e92cccdde60c3d65072a2
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-10-30 21:58:52 +0200

    added necessary dependencies for development

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,23 @@
+import "./index.css";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import {QueryClientProvider,QueryClient} from "@tanstack/react-query";
 
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <p className={"text-red-400"}>hello</p>,
+        errorElement: <div>404 not found</div>
+    }
+])
+    const query = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+     <QueryClientProvider client={query}>
+        <RouterProvider router={router}/>
+        <App />
+     </QueryClientProvider>
   </StrictMode>,
 )
