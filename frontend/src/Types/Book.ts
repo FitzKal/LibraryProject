1825091f0a01b670efe99a9e3b9947018ad6935a
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-01 18:18:39 +0200

    Implemented book type

--- frontend/src/Types/Book.ts
+++ frontend/src/Types/Book.ts
@@ -1,4 +1,9 @@
 
 export interface Book{
-
+    id?:number,
+    title:string,
+    description?:string,
+    pictureSrc?:string,
+    genre?:"FANTASY" | "THRILLER" | "SCIFI" | "ROMANCE",
+    username:string
 }
