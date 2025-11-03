95d73af83ec08c5683ef13806c73e5c20937d38c
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-03 22:39:31 +0100

    implemented all book managing methods except update

--- frontend/src/Components/Auth/RegisterForm.tsx
+++ frontend/src/Components/Auth/RegisterForm.tsx
@@ -48,7 +48,7 @@ export default function RegisterForm(){
                                 value: 4,
                                 message: "The username should be at least 4 characters long"
                             }
-                        })} type={"text"} placeholder={"Username"} className={"bg-white"}/>
+                        })} type={"text"} placeholder={"Username"} className={"textInput bg-white"}/>
                         {errors.username &&( <div className={"text-red-500"}>{errors.username.message}</div>)}
                         <input {...register("password",{
                             required :"Password is required",
--- frontend/src/Services/BookService.ts
+++ frontend/src/Services/BookService.ts
@@ -1,4 +1,6 @@
 // ------------- GetAll -------------
+import type {BookRequest} from "../Types/FormTypes.ts";
+
 export const getAllBooks = async (accessToken:string) =>{
     const res = await fetch("/api/books",{
         headers:{Authorization:`Bearer ${accessToken}`},
@@ -10,5 +12,58 @@ export const getAllBooks = async (accessToken:string) =>{
     }else {
         const message = await res.text();
         throw new Error(message || "Request could not be completed");
+    }
+}
+
+// ------------- GetById -------------
+
+export const getBookById = async (accessToken:string, bookId: number | undefined) =>{
+    const res = await fetch(`/api/books/${bookId}`,{
+        headers: {Authorization : `Bearer ${accessToken}`},
+    });
+    if (res.ok){
+        const response = await res.json();
+        console.log(response);
+        return response;
+    }else {
+        const message = await res.text();
+        throw new Error(message || "Request could not be completed");
+    }
+}
+
+// ------------- PostBook -------------
+
+export const postBook = async (accessToken:string,data:BookRequest, username:string) =>{
+    data.username = username;
+    const res = await fetch("/api/books",{
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type" : "application/json",
+        },
+        body:JSON.stringify(data)
+    });
+    if (res.ok){
+        const response = await res.json();
+        console.log(response);
+        return response;
+    }else {
+        const message = await res.text();
+        throw new Error(message || "Request could not be completed");
+    }
+}
+
+// ------------- DeleteBook -------------
+
+export const deleteBook = async (accessToken:string ,id:number) => {
+    const res = await fetch(`/api/books/${id}`,{
+        method: "DELETE",
+        headers:{Authorization: `Bearer ${accessToken}`}
+    });
+    if (res.ok){
+        return await res.text();
+    }else{
+        const error = await res.text();
+        throw new Error(error || "Request could not be completed");
     }
 }
