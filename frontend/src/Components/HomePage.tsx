41289bc995c87baf0fc5fd5f7a42f3cfdf0a33e8
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-04 15:33:09 +0100

    Implemented logout functionality

--- frontend/src/Components/HomePage.tsx
+++ frontend/src/Components/HomePage.tsx
@@ -1,12 +1,32 @@
 import {userStore} from "../Stores/UserStore.ts";
 import {useNavigate} from "react-router-dom";
 import type {MouseEventHandler} from "react";
+import {useMutation} from "@tanstack/react-query";
+import {userLogout} from "../Services/AuthService.ts";
+import {toast} from "react-toastify";
 
 export default function HomePage(){
     const navigate = useNavigate();
+    const currentUser = userStore.getState().user;
+
+    const logoutMutation = useMutation({
+        mutationFn: () => userLogout(currentUser.accessToken),
+        onSuccess:() =>{
+            userStore.getState().stateLogout();
+            navigate("/login");
+            toast.success("Successfully logged out!");
+        },
+        onError:(error) => {
+            if (error instanceof Error){
+                toast.error(error.message);
+
+            }else{
+                toast.error("Could not complete request");
+            }
+        }
+    })
     const handleLogout: MouseEventHandler<HTMLButtonElement> = () => {
-        userStore.getState().stateLogout();
-        navigate("/login");
+        logoutMutation.mutate();
     };
 
     return(
--- frontend/src/Services/AuthService.ts
+++ frontend/src/Services/AuthService.ts
@@ -37,3 +37,20 @@ export const userRegister = async (data: UserAuthRequest) =>{
         throw new Error(text || "Request could not be completed");
     }
 }
+
+// ------------- LOGOUT -------------
+export const userLogout = async (accessToken: string) =>{
+    const res = await fetch("/api/authenticate/logout",{
+        method: 'POST',
+        headers:{
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type" : "application/json",
+        },
+    });
+    if(res.ok){
+           return;
+    } else{
+        const message = await res.text();
+        throw new Error(message || "Could not complete request");
+    }
+}
